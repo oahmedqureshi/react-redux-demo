2d7cc5932a77c9a6bf8851ed7dff09843d91331a
Owais Ahmed Qureshi <o.ahmedqureshi@gmail.com>
2018-08-02 00:35:47 +0500

    add link in login component and add not found page

--- src/components/login/Login.component.js
+++ src/components/login/Login.component.js
@@ -1,7 +1,11 @@
 import React from "react";
+import {Link} from "react-router-dom"
 
 const Login = () => (
   <div>
+    <Link to='/'>
+      Home
+    </Link>
     My Login Page
   </div>
 )
--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,17 @@
 import React from 'react'
+import {BrowserRouter as Router, Route ,Switch} from 'react-router-dom';
+
+// components
 import Login from "./components/login/Login.component";
 import App from "./components/App";
-import {BrowserRouter as Router, Route ,Switch} from 'react-router-dom';
+import NotFound from "./components/NotFound"
 
 const Routes = () => (
   <Router>
     <Switch>
       <Route exact path='/' component={App}/>
       <Route exact path='/login' component={Login}/>
+      <Route component={NotFound}/>
     </Switch>
   </Router>
 )
